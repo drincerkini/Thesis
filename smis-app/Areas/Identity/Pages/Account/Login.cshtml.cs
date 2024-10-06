9bf02ae9df47e8a7e295233ae7cfc04d6ef1ffb2
drincerkini <93202832+drincerkini@users.noreply.github.com>
2024-10-07 00:51:03 +0200

    Added some more validation to login form

--- smis-app/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ smis-app/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -110,27 +110,16 @@ namespace SchoolManagmentSystem.Areas.Identity.Pages.Account
 
             if (ModelState.IsValid)
             {
-                // This doesn't count login failures towards account lockout
-                // To enable password failures to trigger account lockout, set lockoutOnFailure: true
                 var result = await _signInManager.PasswordSignInAsync(Input.Email, Input.Password, Input.RememberMe, lockoutOnFailure: false);
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User logged in.");
                     return LocalRedirect(returnUrl);
                 }
-                if (result.RequiresTwoFactor)
-                {
-                    return RedirectToPage("./LoginWith2fa", new { ReturnUrl = returnUrl, RememberMe = Input.RememberMe });
-                }
-                if (result.IsLockedOut)
-                {
-                    _logger.LogWarning("User account locked out.");
-                    return RedirectToPage("./Lockout");
-                }
                 else
                 {
-                    ModelState.AddModelError(string.Empty, "Invalid login attempt.");
-                    return Page();
+                    // Generic error message for failed login
+                    ModelState.AddModelError(string.Empty, "Invalid login attempt. Please check your email and password.");
                 }
             }
 
